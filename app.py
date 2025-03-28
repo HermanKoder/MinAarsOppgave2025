558a0c09550b130f1e7312c84f6eafa91c4d10c8
DezyO <145566704+DezyO@users.noreply.github.com>
2025-03-28 11:28:54 +0100

    Fredag 3.28
    blitt ferdig med styling

--- app.py
+++ app.py
@@ -6,6 +6,14 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+@app.route("/game")
+def game():
+    return render_template("game.html")
+
 @app.route("/register")
 def register():
     return render_template("register.html")
--- static/script.js
+++ static/script.js
@@ -1,8 +1,9 @@
+mangoScoreEl = 0
 var mangoScoreEl = document.getElementById("mangoScore")
 var mangoClickEl = document.getElementById("mangoClick")
 
 mangoClickEl.addEventListener("click", plusPoeng)
 
 function plusPoeng() {
-    
+    mangoScoreEl.innerText = mangoScoreEl++
 }
